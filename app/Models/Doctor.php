f7525b1a5e80f2cbda3f7560959f72b6a28b7eac
Vu My <tranhoangvumy22@gmail.com>
2024-03-01 04:15:42 +0700

    Add Doctor Model

---
+++ app/Models/Doctor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "academic_degree", 
+        "specialization_id", 
+        "experience_year",
+        "user_id",
+        "title",
+        "note",
+        "introduction",
+        "training_process",
+        "experience_list",
+        "prize_and_research",
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function specializations(){
+        return $this->belongsToMany(Specialization::class);
+    }
+
+}
---
+++ database/migrations/2024_02_29_203346_create_doctors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->integer("specialization_id");
+            $table->integer("user_id");
+            $table->string("academic_degree")->nullable(); 
+            $table->integer("experience_year")->nullable(); 
+            $table->string("title")->nullable();
+            $table->text("note")->nullable(); 
+            $table->text("introduction")->nullable();
+            $table->text("training_process")->nullable();
+            $table->text("experience_list")->nullable();
+            $table->text("prize_and_research")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+
+};
